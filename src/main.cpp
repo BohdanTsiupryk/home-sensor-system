0edc9232b3f33bed0bf4b0d9f0b62ac4fa022cfd
Bohdan Tsiupryk <bohdan.tsiupryk12@gmail.com>
2020-09-25 23:40:24 +0300

    rewrite on new version nano board

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,49 @@
 #include <HomeSensor.h>
 #include <RF24.h>
 #include <SPI.h>
-#include <Alarm.h>
+#include <Alarm.h> 
+#include <GyverButton.h>
 
+//INITIALIZATION BASE DEFINE-------------------------------------------------------------
 #define ARRAY_SIZE 4
-#define TRANSMITED_DATA_SIZE 32
-#define CHANNEL 0x70
 
-#define LED LED_BUILTIN
+#define RED_LED 10
+#define BLUE_LED 11
 #define BUTTON 5
-#define SENSOR_1 1
-#define SENSOR_2 2
-#define SENSOR_3 3
-#define SENSOR_4 4
-#define BUZZER 6
+#define BUZZER 12
+#define SENSOR_1 A1
+#define SENSOR_2 A2
+#define SENSOR_3 A3
+#define SENSOR_4 A4
+//END INITIALIZATION-------------------------------------------------------------
 
+//VARIABLES -------------------------------------------------------------
 SoftwareSerial rfid(7, 8); // RX, TX
-RF24 radio(9,10);
+GButton button(BUTTON);
 
 char* key[] = {"5300AA56E04F", "5300AC75A62C"};
-byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"};
-byte notTramsmitedData[TRANSMITED_DATA_SIZE];
 
-bool signOn = false;
+bool alarmWork = false;
+bool alarmStatus = false;
 bool alarm = false;
-const byte DEVICE_ID = 2;
+
+int64_t timer = 10000;
+int64_t lastAlarm = 0;
 
 Alarm alarmPlayer(BUZZER);
 
-HomeSensor SR501_1("SR501_1", 10, true);
-HomeSensor SR501_2("SR501_2", 11, true);
-HomeSensor DOOR_1("DOOR_1", 4, true);
-HomeSensor DOOR_2("DOOR_2", 3, true);
+HomeSensor SR501_1("SR501_1", SENSOR_1, false);
+HomeSensor SR501_2("SR501_2", SENSOR_2, false);
+HomeSensor DOOR_1("DOOR_1", SENSOR_3, true);
+HomeSensor DOOR_2("DOOR_2", SENSOR_4, true);
 HomeSensor sensors[ARRAY_SIZE] = {SR501_1, SR501_2, DOOR_1, DOOR_2};
+//END VARIABLES -------------------------------------------------------------
 
 void checkRfid() {
-	if (rfid.available() && rfid.find(key[0])) {
-		Serial.println("SIGNALIATION OFF");
-		alarm = false;
-		digitalWrite(LED, LOW);
+	if (rfid.available() && 
+			(rfid.find(key[0]) || rfid.find(key[1]))) {
+		alarmWork = false;
+		alarmPlayer.stop();
 		rfid.end();
 	}
 }
@@ -49,65 +54,51 @@ void checkSensors() {
 	for (size_t i = 0; i < ARRAY_SIZE; i++) {
 		if (sensors[i].checkSensor()) {
 			sensors[i].worked();
-			alarm = true;	
+			alarmStatus = true;	
+			lastAlarm = millis();
 		}
 	}
 }
 
-void radioSetup() {
-	radio.begin();
- 	radio.setAutoAck(1);         
- 	radio.setRetries(1, 15);    
- 	radio.enableAckPayload();    
-	radio.setPayloadSize(32);     
-
-	radio.openReadingPipe(1, address[0]);   
-	radio.setChannel(CHANNEL);  
-
-	radio.setPALevel(RF24_PA_MAX);
-	radio.setDataRate(RF24_250KBPS); 
-
-	radio.powerUp();
-	radio.startListening();
-
-	Serial.println("Finished config nRF on chanel - " + CHANNEL);
-}
-
-void sendData(byte data[]) {
-	radio.stopListening();
-	delay(20);
-	radio.openWritingPipe(address[0]);
-	radio.write(data, sizeof(data));
-	radio.startListening();
-	delay(30);
-}
-
-void formAndSendData() {
-	byte data[6];
-	data[0] = DEVICE_ID;
-	data[1] = alarm;
-	for (byte i = 0; i < ARRAY_SIZE; i++) {
-		data[i + 2] = sensors[i].getStatus();
+void alarmed() {
+	if (alarmStatus) {
+		if (millis() - lastAlarm > timer) {
+			alarmStatus = false;
+			alarm = true;	
+			alarmPlayer.startPlay();
+		}
 	}
 	
-	sendData(data);
+}
+
+void ledControl() {
+	if (alarmWork) {
+		digitalWrite(RED_LED, HIGH);
+		digitalWrite(BLUE_LED, LOW);
+	} else {
+		digitalWrite(RED_LED, LOW);
+		digitalWrite(BLUE_LED, HIGH);
+	}
 }
 
 void setup() {
-	Serial.begin(9600);
 	rfid.begin(9600);
-	radioSetup();
-	Serial.println("start");
+	pinMode(RED_LED, OUTPUT);
+	pinMode(BLUE_LED, OUTPUT);
+	pinMode(BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-	if (alarm) {
+	if (alarmWork) {
 		checkRfid();
-		alarmPlayer.startPlay();
-	} else {
 		checkSensors();
-		alarmPlayer.stop();
+		alarmed();
+	} else {
+		if (button.isStep()) {
+			alarmWork = true;
+		}	
 	}
+
+	ledControl();
 	alarmPlayer.play();
-	// formAndSendData();
 }
